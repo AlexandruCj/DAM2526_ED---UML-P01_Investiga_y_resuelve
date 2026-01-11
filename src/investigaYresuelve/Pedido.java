339626e4f9092a7ebc3ccd9cc979515f41c62290
pbazcoj2912 <pbazcoj2912@g.educaand.es>
2026-01-11 20:17:12 +0100

    comit casa 2, guardado anterior fallido

--- src/investigaYresuelve/Pedido.java
+++ src/investigaYresuelve/Pedido.java
@@ -1,5 +1,31 @@
 package investigaYresuelve;
 
 public class Pedido {
-	
+	private int numero;
+	private EstadoPedido estado;
+	private List<Producto> productos;
+
+	public Pedido(int numero) {
+
+	}
+
+	public void agregarProducto(Producto p) {
+
+	}
+
+	public double calcularTotal() {
+
+	}
+
+	public int getNumero() {
+
+	}
+
+	public EstadoPedido getEstado() {
+
+	}
+
+	public void setEstado(EstadoPedido estado) {
+
+	}
 }
--- src/investigaYresuelve/Producto.java
+++ src/investigaYresuelve/Producto.java
@@ -1,5 +1,18 @@
 package investigaYresuelve;
 
 public class Producto {
+	private String nombre;
+	private double precio;
 
+	public Producto(String nombre, double precio) {
+
+	}
+
+	public String getNombre() {
+
+	}
+
+	public double getPrecio() {
+
+	}
 }
