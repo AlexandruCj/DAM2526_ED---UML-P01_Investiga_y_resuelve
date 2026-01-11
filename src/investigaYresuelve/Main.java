dbf548207a335e347be4fbb9fb8e53d58fdac895
pbazcoj2912 <pbazcoj2912@g.educaand.es>
2026-01-11 21:16:40 +0100

    commit casa final 11/01/2026

--- src/investigaYresuelve/Main.java
+++ src/investigaYresuelve/Main.java
@@ -1,10 +1,20 @@
 package investigaYresuelve;
 
-import java.util.ArrayList;
-
 public class Main {
-	Pedido pedido = new Pedido(1);
+	public static void main(String[] args) {
+		Pedido pedido1 = new Pedido(1);
 
-	ArrayList<String> producto = new ArrayList<String>();
+		Producto teclado = new Producto("Teclado", 25.50);
+		Producto raton = new Producto("Ratón", 15.00);
 
+		pedido1.agregarProducto(teclado);
+		pedido1.agregarProducto(raton);
+
+		pedido1.setEstado(EstadoPedido.ENVIADO);
+
+		System.out.println("Número del pedido: " + pedido1.getNumero());
+		System.out.println("Estado del pedido: " + pedido1.getEstado());
+		System.out.println("Total del pedido: " + pedido1.calcularTotal());
+		System.out.println("El estado del producto es: " + pedido1.getEstado());
+	}
 }
